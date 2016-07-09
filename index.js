250fec1db1bd126c4fc78a39b7172e1e3f33b9a4
Edgar Li <liedgar7@gmail.com>
2016-07-09 19:25:42 -0400

    Removed cloud code for now

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ if (!databaseUri) {
 
 var api = new ParseServer({
     databaseURI: databaseUri || process.env.DATABASE_URI,
-    cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+    // cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
     appId: process.env.APP_ID || 'myAppId',
     masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
     serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // Don't forget to change to https if needed
