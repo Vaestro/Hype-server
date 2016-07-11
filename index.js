9a1db6a9af6f40608302b3c6145345b13506cf2e
Edgar Li <liedgar7@gmail.com>
2016-07-11 12:36:52 -0400

    Added file key

--- index.js
+++ index.js
@@ -17,11 +17,9 @@ var api = new ParseServer({
     databaseURI: databaseUri || process.env.DATABASE_URI,
     // cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
     appId: process.env.APP_ID || 'myAppId',
+    fileKey: process.env.FILE_KEY || 'myAppId',
     masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
     serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // Don't forget to change to https if needed
-    liveQuery: {
-        classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-    }
 });
 
 var dashboard = new ParseDashboard({
